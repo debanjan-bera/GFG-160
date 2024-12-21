19907856b39103eae38fda18d799514c1b57990b
debanjan-bera <debanjan.bera25discord@gmail.com>
2024-12-21 12:43:53 +0530

    gfg update

---
+++ Searching/Day 22/Find H-Index.py
@@ -0,0 +1,19 @@
+class Solution:
+    # Function to find hIndex
+    def hIndex(self, citations):
+        #code here
+        n = len(citations)
+        buckets = [0] * (n + 1)
+
+        for c in citations:
+            if c >= n:
+                buckets[n] += 1
+            else:
+                buckets[c] += 1
+
+        cumulative = 0
+        for i in range(n, -1, -1):
+            cumulative += buckets[i]
+            if cumulative >= i:
+                return i
+        return 0
