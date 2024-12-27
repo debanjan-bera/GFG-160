4579531873da467d460643267abdd4cf4f2b4e53
debanjan-bera <debanjan.bera25discord@gmail.com>
2024-12-27 14:00:49 +0530

    gfg 160

---
+++ Searching/Day29_Sorted and Rotated Minimum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMin(self, arr):
+        #complete the function here
+        lo, hi = 0, len(arr) - 1
+        while lo < hi:
+            if arr[lo] < arr[hi]:
+                return arr[lo]
+            mid = lo + ((hi - lo) // 2)
+            if arr[mid] > arr[hi]:
+                lo = mid + 1
+            else:
+                hi = mid
+        return arr[lo]
