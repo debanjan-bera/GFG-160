52848f3eb4a326cd897e0b20443536919405b95d
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-01-04 18:36:01 +0530

    gfg 160 updates

---
+++ Matrix/Day2_Rotate by 90 degree.py
@@ -0,0 +1,15 @@
+class Solution:
+    
+    #Function to rotate matrix anticlockwise by 90 degrees.
+    def rotateby90(self, mat): 
+        # code here
+        n = len(mat)
+        for i in range(n):
+            for j in range(i, n):
+                mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+        for j in range(n):
+            i, k = 0, n-1
+            while i < k:
+                mat[i][j], mat[k][j] = mat[k][j], mat[i][j]
+                i += 1
+                k -= 1
