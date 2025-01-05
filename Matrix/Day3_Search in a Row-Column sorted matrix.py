46056215e607e941e33bad097a9dd7b14c9ac92c
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-01-06 00:05:26 +0530

    gfg 160 days update

---
+++ Matrix/Day3_Search in a Row-Column sorted matrix.py
@@ -0,0 +1,12 @@
+class Solution:
+	def matSearch(self, mat, x):
+		# Complete this function
+		r, c = 0, len(mat[0]) - 1
+		while r < len(mat) and c >= 0:
+		    if mat[r][c] == x: 
+				return True
+			
+		    elif mat[r][c] > x: 
+				c -= 1
+		    else: r += 1
+		return False    
