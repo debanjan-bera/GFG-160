b510ec3a75f99b59aba67ae514635d4d4711266e
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-02-10 22:35:40 +0530

    Add solution for counting subarrays with given XOR

---
+++ Hashing/Day10_Count Subarrays.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subarrayXor(self, arr, k):
+        # code here
+        res, prefXOR = 0,0
+        count = {0:1}
+        for val in arr:
+            prefXOR ^=val
+            res += count.get(prefXOR ^ k, 0)
+            count[prefXOR] = count.get(prefXOR, 0) + 1
+        return res
